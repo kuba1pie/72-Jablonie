64c2cc1382222d10a5f499b22099f60f06b8ee9f
kuba1pie <kuba1pie@gmail.com>
2022-05-30 02:16:57 +0200

    Pages templates

---
+++ dist/page-menu.php
@@ -0,0 +1,35 @@
+<?php
+ 
+get_header();
+ 
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
+<article class="page-layout">
+<section class="menu" id="menu">
+
+  <h2>Menu</h2>
+  <div class="wrapper">
+
+    <?php
+
+  $categories = array("starter","soups", "main","desserts", "cold", "hot","drinks");
+ 
+foreach ( $categories as $category ) {
+      get_template_part('dish', null, array('meal' => ( $category ))); 
+
+}
+?>
+  </div>
+
+</section>
+</article>
+
+<?php endwhile;
+ 
+else :
+	echo '<p>There are no pages!</p>';
+endif;
+//get_template_part('aside');
+get_footer();
+ 
+?>
