5bdbca23f8b678c26be3caebe63e3285fbb777c4
juliarup <76255577+juliarup@users.noreply.github.com>
2022-06-01 13:25:46 +0200

    new layout

--- dist/footer.php
+++ dist/footer.php
@@ -2,33 +2,15 @@
 
 <footer>
   <section class="info" id="kontakt">
-    <div class=" opening">
-      <h4>Godziny otwarcia</h4>
-      <p>Śniadania<br> 6:15 - 9:00</p>
-      <p>Restauracja<br> 12:00 - 21:00</p>
-    </div>
-    <div class="adress">
-      <h4>
-        Adres
-      </h4>
-        <p>
-          Restauracja Jabłonie<br>
-          <a href="https://zajazdblonie.pl">Zajazd Błonie</a><br>
-           <a href="https://goo.gl/maps/Ydvhm1fYNRQsJm3c8">
-            📍 ul. Sochaczewska 33A<br>
-            05-870 Błonie
-          </a>
-        </p>
-    </div>
-    <div class="contact">
-      <h4>Kontakt</h4>
-      <p>
-        <a href="tel:+48604530134"> &#128241; +48 604 530 134</a><br>
-        <a href="tel:+48227254561"> &#9742; 22 725 45 61</a><br>
-        <a href="mailto:kontakt@restauracjajablonie.pl">kontakt@restauracjajablonie.pl</a>
-        <a href="mailto:recepcja@zajazdblonie.pl">recepcja@zajazdblonie.pl</a>
-      </p>
-    </div>
+    <h4>Serdecznie zapraszamy!</h4>
+    <p>
+      Sochaczewska 33A, Błonie<br>
+      <a href="tel:+48604530134"> &#128241; +48 604 530 134</a><br>
+      <a href="mailto:kontakt@restauracjajablonie.pl">kontakt@restauracjajablonie.pl</a>
+    </p>
+    <p>
+      Codziennie 12-21
+    </p>
   </section>
   <section class="description">
     Restauracja Jabłonie 2022
